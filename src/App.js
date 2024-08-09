8fa439500012475710259a75c9364c0615687a93
farouk15160 <farouk15160@gmail.com>
2024-08-09 20:51:59 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ThreeDModel from "./components/ThreeDModel";
+import { Flex } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Flex w="100%" h="100vh">
+      <ThreeDModel />
+    </Flex>
+  );
+}
+
+export default App;
